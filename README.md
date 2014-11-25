956100acd84b15a637df2c62469907f671fc0b7e
Andy Librian <andylibrian@gmail.com>
2014-11-25 11:40:01 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Sastrawi String Span
+====================
+
+Sastrawi String Span adalah library span yang digunakan oleh library NLP (Natural Language Processing) Sastrawi lainnya.
+
+
+Cara Install
+-------------
+
+Sastrawi Sentence Detector dapat diinstall dengan [Composer](https://getcomposer.org).
+
+```bash
+php composer.phar require sastrawi/string-span:~1
+```
+
+Lisensi
+--------
+
+Sastrawi Sentence Detector dirilis di bawah lisensi MIT License (MIT).
---
+++ src/Sastrawi/String/Span/Span.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Sastrawi Span (https://github.com/sastrawi/string-span)
+ *
+ * @link      http://github.com/sastrawi/sentence-detector for the canonical source repository
+ * @license   https://github.com/sastrawi/sentence-detector/blob/master/LICENSE The MIT License (MIT)
+ */
+
+namespace Sastrawi\String\Span;
+
+/**
+ * Text span model
+ *
+ * @author Andy Librian
+ */
+class Span
+{
+    private $start = 0;
+
+    private $end = 0;
+
+    private $type;
+
+    public function __construct($start, $end, $type = null)
+    {
+        if (!is_int($start) || $start < 0) {
+            throw new \InvalidArgumentException("start index must be zero or greater: $start given.");
+        }
+
+        if (!is_int($end) || $end < 0) {
+            throw new \InvalidArgumentException("end index must be zero or greater: $end given.");
+        }
+
+        if ($start > $end) {
+            throw new \InvalidArgumentException(
+                "start index can not be greater than end index: start=$start, end=$end"
+            );
+        }
+
+        $this->start = $start;
+        $this->end   = $end;
+        $this->type  = $type;
+    }
+
+    /**
+     * Get the start position.
+     *
+     * @return int
+     */
+    public function getStart()
+    {
+        return $this->start;
+    }
+
+    /**
+     * Get the end position.
+     *
+     * @return int
+     */
+    public function getEnd()
+    {
+        return $this->end;
+    }
+
+    /**
+     * Get type of the span.
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Get the span length
+     *
+     * @return int
+     */
+    public function getLength()
+    {
+        return $this->end - $this->start;
+    }
+
+    /**
+     * Get covered text by this span.
+     *
+     * @param  string $text The text
+     * @return string
+     */
+    public function getCoveredText($text)
+    {
+        return substr($text, $this->start, $this->getLength());
+    }
+}
